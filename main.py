11a735f93198a9662a849656c90c9138e78fc27c
BotLauri <jonas7654@live.se>
2022-12-01 21:17:02 +0900

    Added visualization.
    Now plots the oscillators and their connections.

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ import plot
 import optimize
 
 # TODO: 
-# Start with small network. (N = 3, 4, 5)
+# Try all possibilites for N = 3,4. Find which setups are optimal. Different alpha=0.1, 0.2?
 # Change omega to uniformly spaced omega. 
 # Minimum number of connections is N-1. 
-# Connected network -> eigenvalue of the laplacian. Is connected one the best? Look at printscreen. 
-# One 0 = connected. Look it up? 
-# Link in slack. 
-# Alpha = 0. C = 10/100. 
+# Do we get series or branching network for N=4? (N!/2)
+# Sort different networks by their optimal r values. 
+# Later maybe look at C?
 
 # General parameters.
-N = 7 # Number of oscillators. 
-C = 6*2 # Number of connections. 
+N = 20 # Number of oscillators. 
+C = 19*2 # Number of connections. 
 K = C*2 # Coupling constant. 
 alpha = 0.0 # Phase shift. 
 t_end = 200 # Calculation terminates at t = t_end.
 steps = 1000 # Number of time steps in simulation. 
-iterations = 25 # Optimization iterations. 
-is_directed = False
+iterations = 15 # Optimization iterations. 
+is_directed = True
 
 # Initialization.
 #seed = 2022
@@ -32,7 +31,7 @@ is_directed = False
 rng = np.random.default_rng()
 # Initial values of omega follow normal distribution with mean = 0 and sd = 1.
 #omega = rng.normal(loc=0, scale=1, size=N)
-omega = np.linspace(0, 0.5*N, num=N)
+omega = np.linspace(0, 0.1*N, num=N)
 # Initial values of phi follow uniform distribution. 
 ic = rng.random(size=(N,)) * 2 * np.pi
 
@@ -50,7 +49,8 @@ print(eigenvalues)
 t_eval = np.linspace(0, t_end, steps)
 res = solve_ivp(fun=optimize.kuramoto_network_model, args=(N, K, C, omega, alpha, best_A), y0=ic, 
                     t_span=(0, t_end), t_eval=t_eval, atol=1e-8, rtol=1e-8)
-                        
-#plot.coupling(res, element_avg, N, C, K, alpha)
+
+#plot.coupling(res, steps, N, C, K, alpha)
 #plot.avg_r(iterations, avg_r_hist)
 #plot.best_r(iterations, best_r_hist)
+plot.graph(best_A, N, is_directed)
--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 from matplotlib import pyplot as plt
+import networkx as nx 
 
-def coupling(res, element_avg, N, C, K, alpha):
+def coupling(res, steps, N, C, K, alpha):
+    element_avg = int(steps*0.5) # Number of elements in the averaging over time (r_avg).
     t = res.t # history of time
     theta_hist = res.y # history of theta
     op_hist = np.exp(1j * res.y).sum(axis=0) / N # history of order parameter
@@ -26,3 +28,24 @@ def best_r(iterations, best_r_hist):
     plt.xlabel('iteration',fontsize=18)
     plt.ylabel('best_r',fontsize=18)
     plt.show()
+
+def graph(A, N, is_directed):
+    G = nx.DiGraph() 
+    for i in range(N):
+        G.add_node(i)
+        for j in range(N):
+            if A[i][j] != 0: 
+                G.add_edge(i, j, weight = A[i][j])
+
+    plt.title('Graph displaying the oscillators and their repective connections.')
+    if is_directed:
+        nx.draw_shell(G, with_labels=True, font_weight='bold')
+    else:
+        nx.draw_kamada_kawai(G, with_labels=True, font_weight='bold')
+    #nx.draw_kamada_kawai(G, with_labels=True, font_weight='bold')
+    #nx.draw_planar(G, with_labels=True, font_weight='bold')
+    #nx.draw_spring(G, with_labels=True, font_weight='bold')
+    #nx.draw_shell(G, with_labels=True, font_weight='bold')
+    plt.show()
+    #plt.savefig("path.png")
+    
